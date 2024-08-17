fd096799f1b4a11af9b2b3a028b0eabd4b8d90f1
Arjun G. Menon <contact@arjungmenon.com>
2024-08-17 17:32:17 -0400

    Add chumsky as a dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use chumsky::prelude::*;
+
 fn main() {
     println!("Felicity 0.1.0 ready.\n");
+
+    let mut line = String::new();
+    loop {
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
+
+        println!("{}", line);
+    }
 }
